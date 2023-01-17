1447a8096fefebc0a8d6aaf08a28e7af34350aa7
kepa-lukasz <lukaszkepa03@gmail.com>
2023-01-17 22:00:19 +0100

    post fixed

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get("/get/randomcity", (req, res) => {
 })
 
 
-app.post("/api/guess", (req, res) => {
+app.post("/post/guess", (req, res) => {
     try{
         let searched =  miasta[req.body.city];
         let attempt =  req.body.attempt; 
